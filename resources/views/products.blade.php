1db31695718d75324ec0e47aee50417e37d4c0fa
davimouravilaca <davimouravilaca@gmail.com>
2025-03-24 23:34:02 -0300

    sistema de rotas e views/ trabalhando com o sistema de templates do blade

---
+++ resources/views/products.blade.php
@@ -0,0 +1,5 @@
+<h1> Produtos </h1>
+
+@if (10 > 5) 
+    <p>A condicao e true</p>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Davi";
+    $idade = 21;
+    // A variável é passada para a view com o nome da chave
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => 'programador']);
+});
+
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+Route::get('/produtos', function() {
+    return view('products');
 });
